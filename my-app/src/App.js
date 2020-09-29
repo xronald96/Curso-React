ad76d06735ee006d4b30b15856cfa2fd6907913c
Ronald <ronald881996@gmail.com>
2020-09-29 14:40:45 +0200

    updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hola
+        <Person name="Ronald" age="24" />
+        <Person name="Juan" age="24">My hobbi</Person>
       </div>
     );
   }

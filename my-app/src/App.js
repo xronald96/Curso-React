68e70400b26f72d8f147b1443f53db0b2b861e80
Ronald <ronald881996@gmail.com>
2020-10-09 15:16:06 +0200

    actus

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hola
-        <Person name="Ronald" age="24" />
-        <Person name="Juan" age="24">My hobbi</Person>
-      </div>
-    );
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manue', age: 23 }
+    ]
+  })
+  const switchNameHanhler = (newName) => {
+    // console.log('Was Clickce')
+    // Dont do thiis  !!! .state.persons[0].name="Ronald"
+    setPersonsState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Cristina', age: 23 }
+      ]
+    })
   }
+  return (
+    <div className="App">
+      Hola
+      <button onClick={switchNameHanhler.bind(this, 'Maxii')}>Switch names</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+        click={switchNameHanhler.bind(this, "Ronald")}
+      />
+      <Person name={personsState.persons[1].name}
+        age={personsState.persons[1].age}>My hobbi</Person>
+    </div>
+  );
 }
 
-export default App;
+export default app;
+
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <p> I'm {props.name} and i`m {props.age}!</p>
+      <p onClick={props.click}> I'm {props.name} and i`m {props.age}!</p>
       <p> {props.children}</p>
     </div>
   )

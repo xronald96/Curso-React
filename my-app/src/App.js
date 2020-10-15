3752c45499e23ed048c1efb942004fe0ea39ecc4
Ronald <ronald881996@gmail.com>
2020-10-15 11:51:13 +0200

    yy

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,68 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manue', age: 23 }
-    ]
-  })
-  const switchNameHanhler = (newName) => {
-    // console.log('Was Clickce')
-    // Dont do thiis  !!! .state.persons[0].name="Ronald"
-    setPersonsState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Cristina', age: 23 }
-      ]
-    })
+    ],
+    showPersons: false
   }
-
-  const newChangedHandler = (event) => {
-    setPersonsState({
+  deletePersonHangler = (index) => {
+    let arrayP = [...this.state.persons];
+    arrayP.splice(index, 1)
+    this.setState({ persons: arrayP })
+  }
+  turningPersons = () => {
+    const value = this.state.showPersons
+    this.setState({ ...this.state, showPersons: !value })
+  }
+  newChangedHandler = (event) => {
+    this.setState({
       persons: [
         { name: event.target.value, age: 28 },
         { name: 'Cristina', age: 23 }
-      ]
+      ],
     })
   }
-  return (
-    <div className="App">
-      Hola
-      <button onClick={switchNameHanhler.bind(this, 'Maxii')}> Switch names </button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-        click={switchNameHanhler.bind(this, "Ronald")}
-        changed={newChangedHandler}
-      />
-      <Person name={personsState.persons[1].name}
-        age={personsState.persons[1].age}>My hobbi</Person>
-    </div>
-  );
+  render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (<div>
+        {
+          this.state.persons.map((it, index) => {
+            return (<Person key={index}
+              name={it.name}
+              age={it.age}
+              click={() => this.deletePersonHangler(index)}
+              changed={this.newChangedHandler}
+            />)
+          })
+        }
+        {/* <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHanhler.bind(this, "Ronald")}
+          changed={this.newChangedHandler}
+        />
+        <Person name={this.state.persons[1].name}
+          age={this.state.persons[1].age}>My hobbi
+      </Person> */}
+      </div>)
+    }
+
+    return (
+      <div className="App">
+        Hola
+        <button onClick={() => this.switchNameHanhler('Maxii')}> Switch names </button>
+        <button onClick={this.turningPersons}>Turn on/off</button>
+        {persons}
+      </div>
+    );
+  }
+
 }
 
-export default app;
+export default App;
 
